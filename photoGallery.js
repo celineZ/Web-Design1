37f442dea37eb365a5e7a4e7fd16bb0de3833e45
celineZ <celine3729@yahoo.com>
2016-07-20 15:59:19 -0500

    Add files via upload

---
+++ photoGallery.js
@@ -0,0 +1,25 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    document.getElementById('message').innerHTML = element.alt;
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  
+	}
+
+function unDo(){
+	document.getElementById('message').innerHTML='Hover an image';
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+		
+	}
